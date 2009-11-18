619411f3f5832f0cdd3a56a630e962336c6cba8d
Chris Dinn <chris@massaudience.ca>
2009-11-18 14:48:24 -0500

    Simplify Pamphlet::Base test. Remove settings.yml from repository.

--- test/base_test.rb
+++ test/base_test.rb
@@ -3,9 +3,8 @@ require 'test_helper'
 class BaseTest < Test::Unit::TestCase
   
   def test_settings_load
-    activation_code_digest = Digest::SHA256.hexdigest("abcd1234" + Pamphlet::ACTIVATION_SALT)
-    File.open(Pamphlet::SETTINGS_FILE, 'w') {|f| f.write({'activation_code_digest' => activation_code_digest}.to_yaml)}
-    assert_equal activation_code_digest, Pamphlet.settings['activation_code_digest']
+    File.open(Pamphlet::SETTINGS_FILE, 'w') {|f| f.write({'test_setting' => "this is a test setting"}.to_yaml)}
+    assert_equal "this is a test setting", Pamphlet.settings['test_setting']
   end
   
 end
